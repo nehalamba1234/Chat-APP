e2c9c2bf7591045b3b865d558b54c9bb12afd7d1
21-Mohit <palmohit897@gmail.com>
2024-10-14 01:13:20 +0530

    Updated Code to add clientId to each client and also show all the messages for conversation

--- Src/index.js
+++ Src/index.js
@@ -1,49 +1,35 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const ws_1 = __importStar(require("ws"));
-const http_1 = __importDefault(require("http"));
-const server = http_1.default.createServer(function (request, response) {
-    console.log((new Date()) + 'request come from ' + request.url);
+var WebSocket = require("ws");
+var ws_1 = require("ws");
+var http = require("http");
+var server = http.createServer(function (request, response) {
+    console.log((new Date()) + ' request come from ' + request.url);
     response.end('Hi there');
 });
-const wss = new ws_1.WebSocketServer({ server });
+var wss = new ws_1.WebSocketServer({ server: server });
+var clientIdCounter = 0;
+var clients = new Map();
 wss.on('connection', function connection(ws) {
-    ws.on('error', console.error);
-    ws.on('message', function message(data, isBinary) {
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === ws_1.default.OPEN) {
-                client.send(data, { binary: isBinary });
+    var clientId = clientIdCounter++;
+    clients.set(clientId, ws);
+    console.log("Client ".concat(clientId, " connected."));
+    // Send a welcome message with the client ID  
+    ws.send("Your client ID is ".concat(clientId));
+    ws.on('message', function message(msg) {
+        // Broadcast the message as `clientId msg`  
+        var broadcastMessage = "".concat(clientId, " ").concat(msg);
+        clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(broadcastMessage);
             }
         });
     });
-    ws.send("Hello msg from server");
+    ws.on('close', function () {
+        console.log("Client ".concat(clientId, " disconnected."));
+        clients.delete(clientId);
+    });
 });
 server.listen(8080, function () {
-    console.log((new Date()) + 'server is listening on port  8080');
+    console.log((new Date()) + ' server is listening on port 8080');
 });
--- Src/index.ts
+++ Src/index.ts
@@ -1,28 +1,43 @@
-import WebSocket,{ WebSocketServer } from 'ws';
-import http from 'http'
+import * as WebSocket from 'ws';
+import { WebSocketServer } from 'ws'; 
 
-const server = http.createServer(function(request: any, response:any) {
-    console.log((new Date()) + 'request come from ' + request.url);
-    response.end('Hi there')
+import * as http from 'http'; 
+
+const server = http.createServer((request, response) => {  
+    console.log((new Date()) + ' request come from ' + request.url);  
+    response.end('Hi there');  
+});  
+
+const wss = new WebSocketServer({ server });  
+
+let clientIdCounter = 0;  
+const clients = new Map();  
+
+wss.on('connection', function connection(ws) {  
+    const clientId = clientIdCounter++;  
+    clients.set(clientId, ws);  
+    console.log(`Client ${clientId} connected.`);  
+
+    // Send a welcome message with the client ID  
+    ws.send(`New client Connected with Id as ${clientId}`);  
+
+    ws.on('message', function message(msg) {  
+        // Broadcast the message as `clientId msg`  
+        const broadcastMessage = `${clientId} ${msg}`;  
+
+        clients.forEach(function each(client) {  
+            if (client.readyState === WebSocket.OPEN) {  
+                client.send(broadcastMessage);  
+            }  
+        });  
+    });  
+
+    ws.on('close', function () {  
+        console.log(`Client ${clientId} disconnected.`);  
+        clients.delete(clientId);  
+    });  
+});  
+
+server.listen(8080, () => {  
+    console.log((new Date()) + ' server is listening on port 8080');  
 });
-
-const wss = new WebSocketServer({ server });
-
-wss.on('connection', function connection(ws){
-    ws.on('error',console.error);
-
-    ws.on('message', function message(data , isBinary){
-        wss.clients.forEach(function each(client){
-            if (client.readyState === WebSocket.OPEN){
-                client.send(data,{binary:isBinary});
-            }
-        });
-    });
-    ws.send("Hello msg from server");
-});
-
-server.listen(8080, function(){
-    console.log((new Date())+'server is listening on port  8080');
-
-});
-
