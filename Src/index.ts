29508d965e0d48471bb6594514153f799c106098
Mohit <palmohit897@gmail.com>
2024-10-27 00:40:15 +0530

    Updated code to add count of active user in chat_room

--- Src/index.ts
+++ Src/index.ts
@@ -1,10 +1,9 @@
-import * as WebSocket from 'ws';
-import { WebSocketServer } from 'ws'; 
-
-import * as http from 'http'; 
+import * as WebSocket from 'ws';  
+import { WebSocketServer } from 'ws';   
+import * as http from 'http';   
 
 const server = http.createServer((request, response) => {  
-    console.log((new Date()) + ' request come from ' + request.url);  
+    console.log((new Date()) + ' request from ' + request.url);  
     response.end('Hi there');  
 });  
 
@@ -13,19 +12,32 @@ const wss = new WebSocketServer({ server });
 let clientIdCounter = 0;  
 const clients = new Map();  
 
+// Function to broadcast the current online user count  
+function broadcastOnlineCount() {  
+    const onlineCount = clients.size; // Get the count of connected clients  
+    clients.forEach((client) => {  
+        if (client.readyState === WebSocket.OPEN) {  
+            client.send(`Online Users Count: ${onlineCount}`);  
+        }  
+    });  
+}  
+
 wss.on('connection', function connection(ws) {  
     const clientId = clientIdCounter++;  
     clients.set(clientId, ws);  
     console.log(`Client ${clientId} connected.`);  
 
     // Send a welcome message with the client ID  
-    ws.send(`New client Connected with Id as ${clientId}`);  
+    ws.send(`New client connected with ID: ${clientId}`);  
+
+    // Broadcast the current online count to all clients  
+    broadcastOnlineCount();  
 
     ws.on('message', function message(msg) {  
-        // Broadcast the message as `clientId msg`  
-        const broadcastMessage = `${clientId} ${msg}`;  
+        // Broadcast the received message to all connected clients  
+        const broadcastMessage = `${clientId} says: ${msg}`;  
 
-        clients.forEach(function each(client) {  
+        clients.forEach((client) => {  
             if (client.readyState === WebSocket.OPEN) {  
                 client.send(broadcastMessage);  
             }  
@@ -35,6 +47,9 @@ wss.on('connection', function connection(ws) {
     ws.on('close', function () {  
         console.log(`Client ${clientId} disconnected.`);  
         clients.delete(clientId);  
+        
+        // Broadcast the new online count to all clients  
+        broadcastOnlineCount();  
     });  
 });  
 
