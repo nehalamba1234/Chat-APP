e64b27c5dda1d93dec7504c62c49ef5bd2dbe606
Mohit <palmohit897@gmail.com>
2024-10-27 13:21:55 +0530

    commit in the main branch

--- Src/index.js
+++ Src/index.js
@@ -4,22 +4,33 @@ var WebSocket = require("ws");
 var ws_1 = require("ws");
 var http = require("http");
 var server = http.createServer(function (request, response) {
-    console.log((new Date()) + ' request come from ' + request.url);
+    console.log((new Date()) + ' request from ' + request.url);
     response.end('Hi there');
 });
 var wss = new ws_1.WebSocketServer({ server: server });
 var clientIdCounter = 0;
 var clients = new Map();
+// Function to broadcast the current online user count  
+function broadcastOnlineCount() {
+    var onlineCount = clients.size; // Get the count of connected clients  
+    clients.forEach(function (client) {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send("Online Users Count: ".concat(onlineCount));
+        }
+    });
+}
 wss.on('connection', function connection(ws) {
     var clientId = clientIdCounter++;
     clients.set(clientId, ws);
     console.log("Client ".concat(clientId, " connected."));
     // Send a welcome message with the client ID  
-    ws.send("Your client ID is ".concat(clientId));
+    ws.send("New client connected with ID: ".concat(clientId));
+    // Broadcast the current online count to all clients  
+    broadcastOnlineCount();
     ws.on('message', function message(msg) {
-        // Broadcast the message as `clientId msg`  
-        var broadcastMessage = "".concat(clientId, " ").concat(msg);
-        clients.forEach(function each(client) {
+        // Broadcast the received message to all connected clients  
+        var broadcastMessage = "".concat(clientId, " says: ").concat(msg);
+        clients.forEach(function (client) {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(broadcastMessage);
             }
@@ -28,6 +39,8 @@ wss.on('connection', function connection(ws) {
     ws.on('close', function () {
         console.log("Client ".concat(clientId, " disconnected."));
         clients.delete(clientId);
+        // Broadcast the new online count to all clients  
+        broadcastOnlineCount();
     });
 });
 server.listen(8080, function () {
